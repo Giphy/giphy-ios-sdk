9742b9d999047bda23991d1d4f0664e9962ab505
Christopher Maier <christopher@giphy.com>
2021-08-05 10:35:27 -0700

    create a new project for clips

---
+++ clips-example/clips-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clips-Example
+//
+//  Created by Christopher Maier on 8/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
