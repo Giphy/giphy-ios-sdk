0188230eb1ff9fb6b324292ce9cf63e099ad4bf7
Christopher Maier <christopher@giphy.com>
2023-02-14 11:24:45 -0800

    add bare bones swift ui example project (#220)

---
+++ SwiftUI-Example/SwiftUI-Example/SwiftUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExampleApp.swift
+//  SwiftUI-Example
+//
+//  Created by Christopher Maier on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
